41e2f4057880b98ceaf13b419baac89838202001
wangguanyong <976908722@qq.com>
2017-04-09 13:51:51 +0800

    支付程序

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -12,9 +12,9 @@ return array(
 		//'URL' =>'http://www.test.com', //网站根URL
 		//数据库链接配置
 		'DB_TYPE'   => 'mysql', // 数据库类型
-                'DB_HOST'   => 'localhost', // 服务器地址
+                'DB_HOST'   => '10.66.210.137', // 服务器地址
                 'DB_NAME'   => 'cps', // 数据库名
-                'DB_USER'   => 'cps', // 用户名
+                'DB_USER'   => 'zhuanf_admin', // 用户名
                 'DB_PWD'    => 'L755hKxMmm', // 密码
 		'DB_PORT'   => 3306, // 端口
 		'DB_PREFIX' => 'qw_', // 数据库表前缀
